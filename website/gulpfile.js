348035266d50a09471f3ea41947bb9cc4fde76e5
FangFeiyue <18766131540@163.com>
2018-07-25 09:31:36 +0800

    gulp配置rem

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,4 +1,13 @@
 const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const px2rem = require('postcss-px2rem');
+
 gulp.task('default', function () {
+    const processor = [
+        px2rem({remUnit:75})
+    ];
     
+    return gulp.src('./src/css/mobile.css')
+           .pipe(postcss(processor))
+           .pipe(gulp.dest('./dist/css/'));
 });

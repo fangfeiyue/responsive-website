d95be6b79caab0073ae7168a21d12cbc325a26fe
FangFeiyue <18766131540@163.com>
2018-07-30 13:33:51 +0800

    自动补前缀插件

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const px2rem = require('postcss-px2rem');
+const autoprefixer = require('autoprefixer');
 
 gulp.task('default', function () {
     const processor = [
-        px2rem({remUnit:75})
+        px2rem({remUnit:75}),
+        autoprefixer()
     ];
     
     return gulp.src('./src/css/mobile.css')
